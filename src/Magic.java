068af089b585eef6da0ffd252257366d509924a9
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-02-08 08:05:11 -0500

    Declared variables and initialized scanner

---
+++ src/Magic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Magic {
+
+    public static void main(String[]args){
+        //declare
+        Scanner apex;
+        int value;
+        String question;
+
+        //initialize
+        apex = new Scanner(System.in);
+
+
+
+
+
+
+
+    }
+}
