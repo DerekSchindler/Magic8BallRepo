33fe3f9d8314087c839f058c25b686d91c740634
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-02-11 07:30:35 -0500

    Captured user question and number

--- src/Magic.java
+++ src/Magic.java
@@ -11,6 +11,14 @@ public class Magic {
         //initialize
         apex = new Scanner(System.in);
 
+        System.out.println("Welcome to Magic8Ball. What is your question?");
+        question = apex.nextLine();
+        System.out.println("Please enter a number between 1 and 100.");
+        value = apex.nextInt();
+        
+
+
+
 
 
 
